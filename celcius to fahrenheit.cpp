6876b3d9e0d78b6f24db837b8feb253887994b32
JOHNMOSIOMA <113586253+JOHNMOSIOMA@users.noreply.github.com>
2022-10-06 12:46:40 +0300

    Add files via upload
    conversion of F to celcius

---
+++ celcius to fahrenheit.cpp
@@ -0,0 +1,15 @@
+// C++ program to convert Fahrenheit
+// scale to Celsius scale
+#include <iostream>
+using namespace std;
+ 
+float Conversion(float n)
+{
+    return (n - 32.0) * 5.0 / 9.0;
+}
+int main()
+{
+    float n = 40;
+    cout << Conversion(n);
+    return 0;
+}
