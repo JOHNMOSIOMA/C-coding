a2b728d9fa370e785ee178fd9742999653fd9117
JOHNMOSIOMA <113586253+JOHNMOSIOMA@users.noreply.github.com>
2022-10-06 10:55:41 +0300

    Add files via upload
    sum of number

---
+++ while loop sum.cpp
@@ -0,0 +1,16 @@
+// program for sum of first 10 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,sum=0;
+	cout<<"Enter number till which you would like to add";
+	cin>>n;
+	while(n>0)
+	{
+		sum+=n;
+		n--;
+	}
+	cout<<"\n sum is:"<<sum;
+	return 0;
+}
